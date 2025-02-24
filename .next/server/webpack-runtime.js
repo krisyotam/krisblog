6fc423bde0a8c05a6dca53579d31ed9abceac83c
Kris Yotam <75515498+krisyotam@users.noreply.github.com>
2025-02-24 15:25:46 -0600

    updated about, wishlist, ect.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("342f2c4cdc012b48")
+/******/ 		__webpack_require__.h = () => ("204b406af971061f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "291c49671ea3c305"; }
+/******/ 		__webpack_require__.h = function() { return "e126a99cae48a4cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
